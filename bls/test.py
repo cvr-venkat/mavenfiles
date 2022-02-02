4b4fe4f277e32c01c5b1c09541f8c8fa79f301b0
venkat <chimatavenkat1998@gmail.com>
2022-02-02 17:40:08 -0500

    rel_1.2:bls&gph

--- bls/test.py
+++ bls/test.py
@@ -3,13 +3,23 @@ this is testing process for git
 test feature ()
  pass
 <<<<<<< HEAD
+<<<<<<< HEAD
 
  
  
  
 =======
+=======
+>>>>>>> 238ba21 (rel_1.2:bls&gph)
  
  login ():
 
  logout ():
+<<<<<<< HEAD
 >>>>>>> rel_1.2
+=======
+
+ logs ()
+ log ()
+ 
+>>>>>>> 238ba21 (rel_1.2:bls&gph)
